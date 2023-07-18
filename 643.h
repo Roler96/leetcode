eed9de70f7ac4333f38aa0f4b094625cf37dda2e
roler <roler@aliyun.com>
2023-07-18 11:37:56 +0800

    add 643.h

---
+++ 643.h
@@ -0,0 +1,24 @@
+#include "common.h"
+
+using namespace std;
+
+class Solution {
+public:
+    // 1 <= k <= n <= 1e5
+    double findMaxAverage(vector<int>& nums, int k) {
+        double ans = -1e9;
+        double sum = 0;
+        for (int i=0; i<k; i++)
+            sum += nums[i];
+        ans = max(ans, sum / k);
+        int pos = k;
+        while(pos < nums.size())
+        {
+            sum += nums[pos];
+            sum -= nums[pos - k];
+            ans = max(ans, sum / k);
+            pos++;
+        }
+        return ans;
+    }
+};
