954c519f6f2673eafacdf6fd96001f31708c6842
roler <roler@aliyun.com>
2023-07-18 11:41:42 +0800

    tidy code

--- 643.h
+++ 643.h
@@ -2,17 +2,19 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
     // 1 <= k <= n <= 1e5
-    double findMaxAverage(vector<int>& nums, int k) {
+    double findMaxAverage(vector<int> &nums, int k)
+    {
         double ans = -1e9;
         double sum = 0;
-        for (int i=0; i<k; i++)
+        for (int i = 0; i < k; i++)
             sum += nums[i];
         ans = max(ans, sum / k);
         int pos = k;
-        while(pos < nums.size())
+        while (pos < nums.size())
         {
             sum += nums[pos];
             sum -= nums[pos - k];
