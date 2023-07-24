7692cdb119aadcf9fe0210cba0f31cbd10bea4f4
roler <roler@aliyun.com>
2023-07-25 00:37:19 +0800

    add 933.h

---
+++ 933.h
@@ -0,0 +1,27 @@
+#include "common.h"
+
+using namespace std;
+
+class RecentCounter
+{
+public:
+    RecentCounter()
+    {
+    }
+
+    int ping(int t)
+    {
+        m_queue.push(t);
+        while (t - m_queue.front() > 3000)
+            m_queue.pop();
+    }
+
+private:
+    queue<int> m_queue;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
