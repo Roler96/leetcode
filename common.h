7b412ccdf87b1019407b1cd98495134b398230df
roler <roler@aliyun.com>
2023-07-27 00:31:39 +0800

    Add TreeNode help functions

--- common.h
+++ common.h
@@ -73,3 +73,40 @@ inline void printListNode(ListNode *head)
         head = head->next;
     }
 };
+
+inline TreeNode *initTreeNode(const std::vector<int> &vec, int invalid)
+{
+    if (vec.empty())
+        return nullptr;
+
+    std::vector<TreeNode *> nodeVector;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] != invalid)
+            nodeVector.push_back(new TreeNode(vec[i]));
+        else
+            nodeVector.push_back(nullptr);
+    }
+    for (int i = 0; i < nodeVector.size(); i++)
+    {
+        if (nodeVector[i])
+        {
+            int leftIndex = i * 2 + 1;
+            int rightIndex = (i + 1) * 2;
+            if (leftIndex < nodeVector.size())
+                nodeVector[i]->left = nodeVector[leftIndex];
+            if (rightIndex < nodeVector.size())
+                nodeVector[i]->right = nodeVector[rightIndex];
+        }
+    }
+    return nodeVector[0];
+}
+
+inline void deinitTreeNode(TreeNode *root)
+{
+    if (root == nullptr)
+        return;
+    deinitTreeNode(root->left);
+    deinitTreeNode(root->right);
+    delete root;
+}
